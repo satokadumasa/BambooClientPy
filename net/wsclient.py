19bca50308e35f69ee6a150cb247eeeeed4325b6
satokadumasa <sato.kadumasa@gmail.com>
2019-08-17 04:56:23 +0900

    Websocket送信は可能になる

--- net/wsclient.py
+++ net/wsclient.py
@@ -15,48 +15,46 @@ class WsClient:
     def __init__(self, server_info):
 #             host = "ws://www.hesper.site:3000/"
         print("WsClient.init")
-        print(server_info)
+#         print(server_info)
         self.server_info = server_info
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.session = requests.session()
-        print("WsClient.init CH-01")
-        print("WsClient.init server_name" + self.server_info['servers']['default']['server_name'])
-        print("WsClient.init ws_port" + str(self.server_info['servers']['default']['port']))
-        self.host = "ws://" + self.server_info['servers']['default']['server_name'] + ":" + str(self.server_info['servers']['default']['ws_port']) + "/"
-        print("WsClient.init host:" + self.host)
+#         print("WsClient.init CH-01")
+#         print("WsClient.init server_name" + self.server_info['servers']['default']['server_name'] + "/cable")
+#         print("WsClient.init ws_port" + str(self.server_info['servers']['default']['port']))
+        self.host = "ws://" + self.server_info['servers']['default']['server_name'] + ":" + str(self.server_info['servers']['default']['ws_port']) + "/cable"
+#         print("WsClient.init host:" + self.host)
         self.ws = websocket.WebSocketApp(self.host,
                                     on_message=self.on_message,
                                     on_error=self.on_error,
                                     on_close=self.on_close)
-        print("WsClient.init on_open:")
+#         print("WsClient.init on_open:")
         self.ws.on_open = self.on_open
-        print("WsClient.init run_forever:")
+#         print("WsClient.init run_forever:")
         self.ws.run_forever()
 
-#     def on_message(self, ws, message):
     def on_message(self, message):
         print("on_message")
         print(self.ws)
         print(message)
 
 
-#     def on_error(self, ws, error):
     def on_error(self, error):
         print("on_error")
         print(self.ws)
         print(error)
 
 
-#     def on_close(self, ws):
     def on_close(self):
         print("on_close")
         print(self.ws)
         print("### closed ###")
 
 
-#     def on_open(self, ws):
     def on_open(self):
+        print("###on_open###")
         def run():
+            print("###run###")
             print("run")
             print(self.ws)
             for i in range(3):
@@ -75,10 +73,10 @@ class WsClient:
         print("Thread call run")
         Thread(target=run).start()
 
+    # 認証処理
     def auth_user(self):
         print("Client.auth_user ")
         url = 'https://www.hesper.site/users/sign_in'
-#         session = requests.session()
         response = self.session.get(url)
 
         # BeautifulSoupオブジェクト作成(token取得の為)
---
+++ utils/logger.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 17, 2019
+
+@author: k_sato
+'''
+class Logger:
+    def __init__(self):
+        f = open('tmp/log/debig.log','w')
+        f.write('hoge\n')
+        f.close()
